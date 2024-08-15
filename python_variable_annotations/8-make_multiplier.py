30ea04d8a2816c3dc4f758754eaa8d37e5d22823
Adrybest <adryanbestard@gmail.com>
2024-08-15 22:48:36 +0200

    8. Complex types - functions

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,7 +6,8 @@ float multiplier as argument and returns a function that multiplies a float by m
 from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    def multiplier_function(x: float) -> float:
-        return x * multiplier
     """returns a function that multiplies a float by multiplier"""
+    def multiplier_function(x: float) -> float:
+        """returns the float multiplied by multiplier"""
+        return x * multiplier
     return multiplier_function
