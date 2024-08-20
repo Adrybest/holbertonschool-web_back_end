7f7f7ac55b4f0d8afc095db97753a457f2580cd7
Adrybest <adryanbestard@gmail.com>
2024-08-20 16:37:46 +0200

    0. Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,6 +8,7 @@ import random
 
 
 async def async_generator():
-    for _ in range(10):
+    """coroutine called async_generator that takes no arguments"""
+    for a in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
