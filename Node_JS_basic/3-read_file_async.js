8a906a74f39761a5ee4458d0f0df079362129d58
Adrybest <adryanbestard@gmail.com>
2025-01-24 10:26:21 +0100

    3. Reading a file asynchronously with Node JS

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -10,8 +10,6 @@ function countStudents(path) {
         try {
           const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-          const header = lines.shift();
-
           if (lines.length === 0) {
             throw new Error('No valid students found in the database.');
           }
